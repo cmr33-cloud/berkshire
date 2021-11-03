d82f3f93d161f1c81fd1a84be9b7a9b1aaee32ae
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-11-03 10:39:09 +0000

    dependencies for chart-js

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import {Line} from 'react-chartjs-2'
 
 const Chart = () => {
+
+const data = {
+    labels: ['P1', 'P2', 'P3', 'P4', 'P5'],
+    datasets: [
+        {
+            "label": "Price",
+            "name": "Nikkei",
+            "data": [8.1,8.4,7.8,7.5, 8.2],
+            "rating": 'A'
+          }
+    ]
+}
+
+const options = {
+    scales: {
+      y:  {
+          beginAtZero: true
+      }
+    }
+}
+
     return (
         <div>
-            
+        <Line data={data} options={options} />
         </div>
     )
 }
