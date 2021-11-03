88e89286e41666efa4874be3c4b694350b20bd06
ria10 <ria1099@gmail.com>
2021-11-03 10:55:43 +0000

    added full chart

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import data from './Data'
-import * as V from 'victory'
-import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
+import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis } from 'victory';
 
 const Chart = () => {
     const newData = [{x: Date.now(), y: data[0].price[data[0].price.length-1]},
-                    {x: Date.now(), y: data[0].price[data[0].price.length - 2]}]
+                    {x: 1635936279070, y: data[0].price[data[0].price.length - 2]},
+                    {x: 1635938292070, y: data[0].price[data[0].price.length - 3]}
+                        ]
                 
 
     return (
         <div>
             <VictoryChart>
+            <VictoryAxis 
+            orientation="bottom"
+            standalone={true}
+            fixLabelOverlap={true}
+          />
+          <VictoryAxis dependentAxis
+            orientation="left"
+            standalone={false}
+          />
             <VictoryLine style={{
       data: { stroke: "#c43a31" },
       parent: { border: "1px solid #ccc"}
-    }} data={newData} x="date" y="price"/>
+    }} data={newData} />
             </VictoryChart>
             
         </div>
