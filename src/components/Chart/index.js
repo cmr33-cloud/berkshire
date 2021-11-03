893f3e76221ab36cafd31a7250894706c790f1e5
ria10 <ria1099@gmail.com>
2021-11-03 10:41:17 +0000

    adding chart

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import data from './Data'
+import * as V from 'victory'
+import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 
 const Chart = () => {
+    const newData = [{x: Date.now(), y: data[0].price[data[0].price.length-1]},
+                    {x: Date.now(), y: data[0].price[data[0].price.length - 2]}]
+                
+
     return (
         <div>
+            <VictoryChart>
+            <VictoryLine style={{
+      data: { stroke: "#c43a31" },
+      parent: { border: "1px solid #ccc"}
+    }} data={newData} x="date" y="price"/>
+            </VictoryChart>
             
         </div>
     )
